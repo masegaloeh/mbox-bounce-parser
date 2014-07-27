a17ffb3c573650e726dcdf1fa93cc57222600132
z <z@build.arc.itb.ac.id>
2014-07-27 18:24:43 +0700

    add some rules. Verified by most of emails in ITBCC bounces

--- read_mailbox.py
+++ read_mailbox.py
@@ -1,37 +1,52 @@
 #!/usr/bin/env python3
+
 import argparse
 import mailbox
 import re
 
 def apply_rule(message_part):
     dict_rules = [
-    {'rules': 'This user doesn\'t.+have account', 'reject_type': 'hard'}
+    {'rules': '^smtp;\s*5\d{2}', 'reject_type':'hard'}, #postfix default reply code
+    {'rules': '^smtp;\s*4\d{2}', 'reject_type':'soft'}, #postfix default reply code
+    {'rules': 'delivery temporarily suspended', 'reject_type':'soft'},
+    {'rules': 'over quota', 'reject_type':'soft'}, 
+    {'rules': 'Malformed or unexpected name server reply', 'reject_type':'soft'},
+    {'rules': '(Host|domain).*not found', 'reject_type':'soft'},
+    #{'rules': 'This user doesn\'t.+have.+account', 'reject_type': 'hard'},
+    #{'rules': 'Recipient address rejected', 'reject_type': 'hard'},
+    #{'rules': ' The email account that you tried to reach does not exist', 'reject_type': 'hard'},
+    #{'rules': 'connectWithTimeout failed \[127\.0.\0\.1:628]\: Connection refused', 'reject_type': 'soft'}
     ]
 
+    #print(message_part)
     for xrules in dict_rules:
-        if re.match(xrules['rules'], message_part) is None:
+        if re.search(xrules['rules'], message_part) is not None:
             return xrules['reject_type']
+            #return "| " + message_part
     else:
-        return False
+        return "| " + message_part
 
 def find_payload(message):
+    is_status = False
+
     if message.is_multipart():
-
         #Walking Recursively to search header Diagnostic-Code
-
-        is_status = False
-        for part in message.walk(): 
+        for part in message.walk():
             if part.get_content_type() == 'message/delivery-status':
                 for epart in part.walk():
                     if epart['Diagnostic-Code'] is not None:
                         is_status = True
-                        givenstring = epart['Diagnostic-Code'].replace('\n', ' ')
+                        givenstring = re.sub('\s{2,}', ' ', epart['Diagnostic-Code'])
+                        #givenstring = epart['Diagnostic-Code'].replace('\n', ' ')
                         result = apply_rule(givenstring)
 
-        if not is_status:
-            return False
-        else:
-            return result
+    #else:
+        #result = apply_rule(message.as_string())
+
+    if not is_status:
+        return False
+    else:
+        return result
                 
 
 
@@ -51,6 +66,7 @@ def bounce_parser(message):
     elif mygroup2 is not None:
         origin = mygroup2.groups()[0].replace('=', '@') + " "
     else:
+        print("EMBUH")
         return False
 
     result = find_payload(message)
@@ -69,3 +85,10 @@ args = parser.parse_args()
 mbox = mailbox.mbox(args.input)
 for message in mbox:
     bounce_parser(message)
+
+#user
+# email_verified
+# subscribe
+# subscribe_article
+
+# del all
